fa6121ca87ad8b2c1745bee37228dee8e6fdb6aa
andrey0902 <andreypavlov1706@gmail.com>
2019-01-15 23:24:46 +0200

    first commit create data source class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CoursesService} from './courses.service';
+import {LessonsDataSource} from './data-source';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  dataSource: LessonsDataSource;
   title = 'materialTable';
+  displayedColumns = ['seqNo', 'description', 'duration'];
+
+
+  constructor(private coursesService: CoursesService) {}
+
+  ngOnInit() {
+    this.dataSource = new LessonsDataSource(this.coursesService);
+    this.dataSource.loadLessons(1);
+  }
+
+  onRowClicked(row) {
+    console.log('click on the row', row);
+  }
+
 }
---
+++ src/app/data-source.ts
@@ -0,0 +1,38 @@
+import {DataSource} from '@angular/cdk/table';
+import {BehaviorSubject, Observable, of} from 'rxjs';
+import {CoursesService} from './courses.service';
+import {CollectionViewer} from '@angular/cdk/collections';
+import {catchError, finalize} from 'rxjs/operators';
+import {Lesson} from './model/model';
+
+export class LessonsDataSource implements DataSource<Lesson> {
+
+  private lessonsSubject = new BehaviorSubject<Lesson[]>([]);
+  private loadingSubject = new BehaviorSubject<boolean>(false);
+
+  public loading$ = this.loadingSubject.asObservable();
+
+  constructor(private coursesService: CoursesService) {}
+
+  connect(collectionViewer: CollectionViewer): Observable<Lesson[]> {
+    return this.lessonsSubject.asObservable();
+  }
+
+  disconnect(collectionViewer: CollectionViewer): void {
+    this.lessonsSubject.complete();
+    this.loadingSubject.complete();
+  }
+
+  loadLessons(courseId: number, filter = '',
+              sortDirection = 'asc', pageIndex = 0, pageSize = 3) {
+
+    this.loadingSubject.next(true);
+
+    this.coursesService.findLessons(courseId, filter, sortDirection,
+      pageIndex, pageSize).pipe(
+      catchError(() => of([])),
+      finalize(() => this.loadingSubject.next(false))
+    )
+      .subscribe(lessons => this.lessonsSubject.next(lessons));
+  }
+}
